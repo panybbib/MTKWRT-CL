57881728bed13df697cd7f41c5bcf7673b9d1915
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-04-11 10:31:25 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -41,6 +41,28 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 
+# 更改菜单名字
+echo -e "\nmsgid \"MosDNS\"" >> package/luci-app-mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
+echo -e "msgstr \"转发分流\"" >> package/luci-app-mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
+
+#echo -e "\nmsgid \"Lucky\"" >> package/luci-app-lucky/luci-app-lucky/po/zh_Hans/lucky.po
+#echo -e "msgstr \"大吉大利\"" >> package/luci-app-lucky/luci-app-lucky/po/zh_Hans/lucky.po
+
+#echo -e "\nmsgid \"AList\"" >> package/luci-app-alist/luci-app-alist/po/zh_Hans/alist.po
+#echo -e "msgstr \"聚合网盘\"" >> package/luci-app-alist/luci-app-alist/po/zh_Hans/alist.po
+
+#echo -e "\nmsgid \"Tailscale\"" >> package/luci-app-tailscale/po/zh_Hans/tailscale.po
+#echo -e "msgstr \"虚拟组网\"" >> package/luci-app-tailscale/po/zh_Hans/tailscale.po
+
+#echo -e "\nmsgid \"Nikki\"" >> package/OpenWrt-nikki/luci-app-nikki/po/zh_Hans/nikki.po
+#echo -e "msgstr \"科学上网\"" >> package/OpenWrt-nikki/luci-app-nikki/po/zh_Hans/nikki.po
+
+echo -e "\nmsgid \"HomeProxy\"" >> package/homeproxy/po/zh_Hans/homeproxy.po
+echo -e "msgstr \"科学代理\"" >> package/homeproxy/po/zh_Hans/homeproxy.po
+
+echo -e "\nmsgid \"UPnP\"" >> package/mtk/applications/luci-app-upnp-mtk-adjust/po/zh_Hans/upnp.po
+echo -e "msgstr \"即插即用\"" >> package/mtk/applications/luci-app-upnp-mtk-adjust/po/zh_Hans/upnp.po
+
 #配置文件修改
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
